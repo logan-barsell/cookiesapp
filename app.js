2479445724dd1b6f5b97d81fb266592d1c01bca6
Logan Barsell <loganjbars@gmail.com>
2017-03-03 12:33:00 +0100

    added cookieParser

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cookieParser = require('cookie-parser')
 
 app.use(express.static(__dirname+'/static'))
+app.use(cookieParser())
 
 app.listen(3000, f=> {
 	console.log("Server Running!")
